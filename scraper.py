1a981cd8cbf14dec59383cdaaffdbbaa21edebc7
SaiVenkatesh007 <saivenki65@gmail.com>
2023-01-13 16:04:22 +0530

    Code Cleaned

--- scraper.py
+++ scraper.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
-text_list = []
+text_list = [] # List to store the news titles
 
-for n in range(0, 158):
+# Going through the page
+for n in range(0, 158): # Loop is for going through the pages 159 pages of data were available
     pg = requests.get(f"https://dfrac.org/en/topic/fake/page/{n+1}/")
     cnt = pg.content
 
@@ -14,17 +15,21 @@ for n in range(0, 158):
     for i in headline:
         title = i.find('h3').get_text()
         title = title.strip()
+        # Cleaning the data (Removing Unnecessary Parts)
         title = title.replace("- Read Fact Check", "").replace(" Read- Fact Check", "").replace("Read, Fact-Check", "").replace("Fact Check: ", "").replace(" Read Fact Check", "").replace("Fact Check-", "").replace('FactCheck:', '').replace('Fact-Check: ', '')
         title = title.strip()
         text_list.append(title)
+
 
 data = {
     'Headline' : text_list,
     'Target' : ["false"] * len(text_list),
 }
 
+# Making a datafram of the data
 df = pd.DataFrame(data)
 df = df.drop_duplicates()
 
 
-#df.to_csv("[FILE LOCATION")
+#df.to_csv("[FILE LOCATION]")
+# [FILE LOCATION] --> "data/scrapping.csv"
